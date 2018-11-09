21a4013d48d1b00948edbf700cc95323ca3f227d
Zeztron <pateltharsh@gmail.com>
2018-11-08 20:06:23 -0500

    Added jquery effect on mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,4 +80,18 @@ $(document).ready(function() {
       }, {
           offset: "50%"
       });
+
+      // Mobile navigation //
+      $(".js--nav-icon").click(function () {
+          var nav = $(".js--main-nav");
+          var icon = $(".js--nav-icon i");
+          nav.slideToggle(200);
+          if (icon.hasClass("ion-ios-menu")) {
+              icon.addClass("ion-md-close");
+              icon.removeClass("ion-ios-menu");
+          } else {
+              icon.addClass("ion-ios-menu");
+              icon.removeClass("ion-md-close");
+          }
+      });
 });
